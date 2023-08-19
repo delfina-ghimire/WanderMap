856ace915ea849ab71d4735821835e7439967e56
delfina-ghimire <delfina.ghimire@gmail.com>
2023-08-19 21:11:52 +0545

    Add Pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
-import { PageNotFound } from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+
+// import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
@@ -12,7 +14,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
+        {/* <Route path="app" element={<AppLayout />} /> */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,11 +1,3 @@
-import React from "react";
-import PageNav from "../components/PageNav";
-
-export const PageNotFound = () => {
-  return (
-    <div>
-      <PageNav />
-      PageNotFound
-    </div>
-  );
-};
+export default function PageNotFound() {
+  return <div>PageNotFound</div>;
+}
