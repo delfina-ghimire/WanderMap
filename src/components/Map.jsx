6647d6bac0c8ff5876acc8eca00d09957a99df7f
delfina-ghimire <delfina.ghimire@gmail.com>
2023-09-08 11:31:56 +0545

    query strings and url parameters

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,13 @@
+import { useSearchParams } from "react-router-dom";
 import Styles from "./Map.module.css";
 
 export default function Map() {
-  return <div className={Styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={Styles.mapContainer}>
+      Map position: {lat} and {lng}
+    </div>
+  );
 }
